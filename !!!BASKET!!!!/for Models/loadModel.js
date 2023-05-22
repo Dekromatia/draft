46a960b95b758966a124b722f30e20374627a283
Anastasia Glotova <90776972+Dekromatia@users.noreply.github.com>
2023-05-22 03:57:36 +0300

    Delete !!!BASKET!!!! directory

--- !!!BASKET!!!!/for Models/loadModel.js
+++
@@ -1,6 +0,0 @@
-import { renderModel } from "./renderModel.js";
-
-export function loadModel(scene) {
-  const modelLink = 'https://rssda.su/auxil/ar0535.html'; // replace with your model URL
-  renderModel(scene, modelLink); // call the renderModel function
-}
