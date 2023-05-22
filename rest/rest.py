3594abcf06580107efdc6ee4028da7f5084928d4
Anastasia Glotova <90776972+Dekromatia@users.noreply.github.com>
2023-05-22 03:53:59 +0300

    ignore

--- rest/rest.py
+++
@@ -1,9 +0,0 @@
-from eve_sqlalchemy import SQL as _SQL
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy(app)
-
-class SQL(_SQL):
-   driver = db
-
-app = Eve(validator=ValidatorSQL, data=SQL)
